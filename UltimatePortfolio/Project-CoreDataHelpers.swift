4030c70089d26582582a0c3eae6b8bb13386631d
Dominic Santo <dominic@codexpress.co>
2020-11-23 08:21:55 +0200

    add: tab navigtaion implementation

---
+++ UltimatePortfolio/Project-CoreDataHelpers.swift
@@ -0,0 +1,69 @@
+//
+//  Project-CoreDataHelpers.swift
+//  UltimatePortfolio
+//
+//  Created by Dominic Santo on 2020/11/17.
+//
+
+import Foundation
+
+extension Project {
+    var projectTitle: String {
+        title ?? ""
+    }
+    
+    var projectDetail: String {
+        detail ?? ""
+    }
+    
+    var projectColor: String {
+        color ?? "Light Blue"
+    }
+    
+    var projectItems: [Item] {
+        let itemsArray = items?.allObjects as? [Item] ?? []
+        return itemsArray.sorted { first, second in
+            if first.completed == false {
+                if second.completed == true {
+                    return true
+                }
+            } else if first.completed == true {
+                if second.completed == false {
+                    return false
+                }
+            }
+            
+            if first.priority > second.priority {
+                return true
+            } else if first.priority < second.priority {
+                return false
+            }
+            
+            return first.itemCreationDate < second.itemCreationDate
+        }
+    }
+    
+    var completionAmount: Double {
+        let originalItems = items?.allObjects as? [Item] ?? []
+        guard originalItems.isEmpty == false else { return 0 }
+        
+        let completedItems = originalItems.filter(\.completed)
+        
+        return Double(completedItems.count / originalItems.count)
+    }
+    
+    static var example: Project {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+        
+        let project = Project(context: viewContext)
+        
+        project.title = "Exmaple Project"
+        project.detail = "This is an example project"
+        project.closed = true
+        project.creationDate = Date()
+        
+        return project
+    }
+}
+
